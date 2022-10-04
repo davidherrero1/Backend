4fb2fba8687a4ca46f7e2a504064d36e8279d8fe
david.herrero.faura <david.herrero.faura@est.fib.upc.edu>
2022-10-04 16:51:25 +0000

    Initial commit

---
+++ src/main/java/Electricity/Backend/ElectricityApplication.java
@@ -0,0 +1,13 @@
+package Electricity.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Electricity/Backend/ElectricityApplicationTests.java
@@ -0,0 +1,13 @@
+package Electricity.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
